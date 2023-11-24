bbc069285df91779d6c16abf778d25e1a151c933
Ameer <ameerapex496@gmail.com>
2023-11-24 23:04:09 +0100

    added functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,60 @@
-console.log("hallo world");
+//selecting our elements
+var tagsEl = document.getElementById("choices-Wrap-Id");
+var textEl = document.getElementById("text-Area");
+textEl === null || textEl === void 0 ? void 0 : textEl.focus();
+// our functions
+var createTags = function (e) {
+    var words = e
+        .split(",")
+        .filter(function (tag) { return tag.trim() !== " "; })
+        .map(function (tag) { return tag.trim(); });
+    tagsEl === null || tagsEl === void 0 ? void 0 : tagsEl.innerHTML = "";
+    words.forEach(function (word) {
+        var choiceEl = document.createElement("div");
+        choiceEl.classList.add("choice");
+        choiceEl.innerText = word;
+        tagsEl === null || tagsEl === void 0 ? void 0 : tagsEl.appendChild(choiceEl);
+    });
+};
+// our random select func
+var randdomSelect = function () {
+    var time = 30;
+    var interval = setInterval(function () {
+        var randomTag = pickRandomTag();
+        highlightTag(randomTag);
+        setTimeout(function () {
+            unHighlightTag(randomTag);
+        }, 100);
+    }, 100);
+    setTimeout(function () {
+        clearInterval(interval);
+        setTimeout(function () {
+            var randTag = pickRandomTag();
+            highlightTag(randTag);
+        }, 100);
+    }, time * 100);
+};
+// random tag func :
+var pickRandomTag = function () {
+    var tags = document.querySelectorAll(".choice");
+    return tags[Math.floor(Math.random() * tags.length)];
+};
+// highlightTag func
+var highlightTag = function (tag) {
+    tag.classList.add("active");
+};
+// unHighlightTag  func
+var unHighlightTag = function (tag) {
+    tag.classList.remove("active");
+};
+// our eventlisnters
+textEl === null || textEl === void 0 ? void 0 : textEl.addEventListener("keyup", function (e) {
+    createTags(e.target.value);
+    // our enter function
+    if (e.key === "Enter") {
+        setTimeout(function () {
+            e.target.value = "";
+        }, 10);
+        randdomSelect();
+    }
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,74 @@
-console.log("hallo world");
+//selecting our elements
+const tagsEl = document.getElementById("choices-Wrap-Id");
+const textEl = document.getElementById("text-Area");
+
+textEl?.focus();
+
+// our functions
+const createTags = (e) => {
+  const words = e
+    .split(",")
+    .filter((tag) => tag.trim() !== " ")
+    .map((tag) => tag.trim());
+  tagsEl?.innerHTML = "";
+  words.forEach((word) => {
+    const choiceEl = document.createElement("div");
+    choiceEl.classList.add("choice");
+    choiceEl.innerText = word;
+    tagsEl?.appendChild(choiceEl);
+  });
+};
+
+// our random select func
+const randdomSelect = () => {
+  const time: number = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randTag = pickRandomTag();
+      highlightTag(randTag);
+    }, 100);
+  }, time * 100);
+};
+
+// random tag func :
+const pickRandomTag = () => {
+  const tags = document.querySelectorAll(".choice");
+  return tags[Math.floor(Math.random() * tags.length)];
+};
+
+// highlightTag func
+
+const highlightTag = (tag) => {
+  tag.classList.add("active");
+};
+
+// unHighlightTag  func
+const unHighlightTag = (tag) => {
+  tag.classList.remove("active");
+};
+
+// our eventlisnters
+textEl?.addEventListener("keyup", (e: any) => {
+  createTags(e.target.value);
+
+  // our enter function
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randdomSelect();
+  }
+});
